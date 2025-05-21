a4fa4804f744a4776e144752b586f1be5b3dae73
Abhijeet Kashid <abhijeetkashid2002@gmail.com>
2025-05-22 00:49:04 +0530

    bug-fix-4

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,12 +1,11 @@
-import {connect} from "@/dbConfig/dbConfig";
+import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextResponse, NextRequest } from "next/server";
 import bcryptjs from "bcryptjs";
 
 connect();
 
-
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest): Promise<NextResponse> {
     try {
         const reqBody = await request.json();
         const {username, email, password} = reqBody;
@@ -35,8 +34,8 @@ export async function POST(request: NextRequest) {
             user: savedUser
         });
 
-    } catch (error: any) {
-        return NextResponse.json({error: error.message},
+    } catch (error:  object | unknown) {
+        return NextResponse.json({error: (error as any)?.message},
             {status: 500});
     }
 }
