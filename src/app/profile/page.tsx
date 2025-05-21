82263e5a624c9910e18a363e6aa174810daa29d7
Abhijeet Kashid <abhijeetkashid2002@gmail.com>
2025-05-22 00:29:44 +0530

    bug-fixes-2

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -48,7 +48,7 @@ export default function ProfilePage() {
         className="bg-blue-500 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         type="button"
       >
-        {user?.username}
+        {(user as any)?.username}
       </button>
     </div>
   );
